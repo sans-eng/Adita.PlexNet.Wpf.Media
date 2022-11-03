60430fec6f8cab92e59fdb00d9018f9c1ff3ff90
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-11-03 14:10:08 +0700

    Add project files.

---
+++ Adita.PlexNet.Wpf.Media/MaterialIconKind.cs
@@ -0,0 +1,29 @@
+﻿namespace Adita.PlexNet.Wpf.Media
+{
+    /// <summary>
+    /// Defines the kind of material icon.
+    /// </summary>
+    public enum MaterialIconKind
+    {
+        /// <summary>
+        /// Standard material icon.
+        /// </summary>
+        Standard = 0,
+        /// <summary>
+        /// Outline material icon.
+        /// </summary>
+        Outline,
+        /// <summary>
+        /// Round material icon.
+        /// </summary>
+        Round,
+        /// <summary>
+        /// Sharp material icon.
+        /// </summary>
+        Sharp,
+        /// <summary>
+        /// Two tone material icon.
+        /// </summary>
+        TwoTone
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Adita.PlexNet.Wpf.Media
+
+A media utilities for PlexNet framework that targeting .NET 6.
