6ef33fc11dbe7bff90243c4e5470df74a1f9250a
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-11-06 22:40:23 +0700

    Unified material icon resources constants

---
+++ Adita.PlexNet.Wpf.Media/Internals/MaterialIconResources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adita.PlexNet.Wpf.Media.Internals
+{
+    internal static class MaterialIconResources
+    {
+        #region Internal constants
+        internal const string StandardFontPath = "pack://application:,,,/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIcons-Regular.ttf";
+        internal const string OutlineFontPath = "pack://application:,,,/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIconsOutlined-Regular.otf";
+        internal const string RoundFontPath = "pack://application:,,,/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIconsRound-Regular.otf";
+        internal const string SharpFontPath = "pack://application:,,,/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIconsSharp-Regular.otf";
+        internal const string TwoToneFontPath = "pack://application:,,,/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIconsTwoTone-Regular.otf";
+
+        internal const string FontNamePath = "/Adita.PlexNet.Wpf.Media;component/Resources/Fonts/MaterialIconNames.xaml";
+        #endregion Internal constants
+    }
+}
