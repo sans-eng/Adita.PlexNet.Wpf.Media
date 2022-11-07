a283ece0662e862dd4d0aeda7a53e18589128c17
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-11-07 16:46:31 +0700

    Add license statemen and refactoring param

--- Adita.PlexNet.Wpf.Media/MaterialIconImageSourceExtension.cs
+++ Adita.PlexNet.Wpf.Media/MaterialIconImageSourceExtension.cs
@@ -1,4 +1,26 @@
-﻿using Adita.PlexNet.Wpf.Media.Internals;
+﻿//MIT License
+
+//Copyright (c) 2022 Adita
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using Adita.PlexNet.Wpf.Media.Internals;
 using System;
 using System.Windows;
 using System.Windows.Documents;
@@ -23,7 +45,7 @@ namespace Adita.PlexNet.Wpf.Media
         /// </summary>
         /// <remarks>The name is predefined on system PlexNet framework,
         /// see <a href="https://docs.plexdotnet.adita-engineering.com/wpf/media/material-icon"/> for documentation.</remarks>
-        public string IconName { get; set; } = String.Empty;
+        public string IconName { get; set; } = string.Empty;
         /// <summary>
         /// Gets or sets the kind of the icon.
         /// </summary>
@@ -38,6 +60,7 @@ namespace Adita.PlexNet.Wpf.Media
         /// <summary>Returns the image source value that is provided as the value of the target property for this markup extension.</summary>
         /// <param name="serviceProvider">A service provider helper that can provide services for the markup extension.</param>
         /// <returns>The the image source value to set on the property where the extension is applied.</returns>
+        /// <exception cref="InvalidOperationException">Specified <see cref="IconName"/> is not valid.</exception>
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             return CreateImageSource(IconName, Kind, Brush);
@@ -63,12 +86,12 @@ namespace Adita.PlexNet.Wpf.Media
                 OriginY = 0
             };
 
-            return new DrawingImage(new GlyphRunDrawing(Brush, glyphs.ToGlyphRun()));
-    }
+            return new DrawingImage(new GlyphRunDrawing(brush, glyphs.ToGlyphRun()));
+        }
 
-        private Uri GetFontUri(MaterialIconKind iconKind)
+        private Uri GetFontUri(MaterialIconKind kind)
         {
-            return iconKind switch
+            return kind switch
             {
                 MaterialIconKind.Standard => new(MaterialIconResources.StandardFontPath),
                 MaterialIconKind.Outline => new(MaterialIconResources.OutlineFontPath),
